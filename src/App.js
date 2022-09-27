34bb9a95ea276adb107015d5a43cbcb1266b89f2
Rommel Santos <rommelrenne@gmail.com>
2022-09-27 20:52:47 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  FaChevronLeft,
+  FaSpotify,
+  FaAmazon,
+  FaMusic,
+  FaYoutube,
+} from "react-icons/fa";
+import "./index.css";
 
-function App() {
+export default function App() {
+  function handleInput(e) {
+    if (e.target.value === "spotify") {
+      window.open("https://open.spotify.com/");
+    } else if (e.target.value === "yt-music") {
+      window.open("https://music.youtube.com/");
+    } else if (e.target.value === "amazon") {
+      window.open("https://music.amazon.com/");
+    } else if (e.target.value === "yt") {
+      window.open("https://youtube.com/");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="app-header">
+        <button id="previous">
+          <FaChevronLeft size={25} color="#fff" />
+        </button>
       </header>
-    </div>
+
+      <div className="app">
+        <div id="cover-container">
+          <div id="cover"></div>
+          <p id="playlist-name">Nome da playlist</p>
+        </div>
+
+        <div className="straming-options">
+          <p>Gostaria de ouvir por onde?</p>
+          <button value="spotify" onClick={(e) => handleInput(e, "value")}>
+            <FaSpotify size={20} className="icon" />
+            Spotify
+          </button>
+          <button value="yt-music" onClick={(e) => handleInput(e, "value")}>
+            <FaMusic size={20} className="icon" />
+            Youtube Music
+          </button>
+          <button value="amazon" onClick={(e) => handleInput(e, "value")}>
+            <FaAmazon size={20} className="icon" />
+            Amazon Music
+          </button>
+          <button value="yt" onClick={(e) => handleInput(e, "value")}>
+            <FaYoutube size={20} className="icon" />
+            Youtube
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
-
-export default App;
